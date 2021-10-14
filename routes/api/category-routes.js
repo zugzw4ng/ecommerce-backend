82c61cff6af6fee501e3336f21dfddd755d1cb28
zugzw4ng <bongcloud2001@gmail.com>
2021-10-14 14:28:46 -0400

    added Category findAll method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,19 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  )
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
