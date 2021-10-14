2fc5914719b1548418f8c7209c8895f6df838c91
zugzw4ng <bongcloud2001@gmail.com>
2021-10-14 14:42:31 -0400

    added Category update PUT route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,26 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'No Category found with that ID.' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
