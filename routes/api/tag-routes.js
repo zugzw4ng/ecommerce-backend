b10cdde2bad009f16de4bc721513764c72fe2f31
zugzw4ng <bongcloud2001@gmail.com>
2021-10-14 21:06:12 -0400

    added GET route for all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,8 +4,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll(
+    {
+      include: {
+        model: Product
+      }
+    }
+  )
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
