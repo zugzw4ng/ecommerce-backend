954f407c251fcd0001b4215cc2e651cf16beead7
zugzw4ng <bongcloud2001@gmail.com>
2021-10-14 21:09:39 -0400

    added GET single tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,8 +19,19 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
